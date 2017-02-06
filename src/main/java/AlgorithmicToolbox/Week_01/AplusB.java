af3005e7fba21168b788bf6dbcd81dad80e31389
Izmalkov Roman <rgizmalkov@gmail.com>
2017-02-06 21:12:47 +0300

    Initial commit

---
+++ src/main/java/AlgorithmicToolbox/Week_01/AplusB.java
@@ -0,0 +1,16 @@
+package AlgorithmicToolbox.Week_01;
+
+import java.util.Scanner;
+
+/**
+ * Created by romanizmalkov on 06.02.17.
+ */
+public class AplusB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
