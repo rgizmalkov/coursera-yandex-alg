21fcdec9b254b403b82b362654aa6dade120fc7b
Izmalkov Roman <rgizmalkov@gmail.com>
2017-02-08 17:01:31 +0300

    Pairwise - 17/17

--- src/main/java/AlgorithmicToolbox/Week_01/Pairwise.java
+++ src/main/java/AlgorithmicToolbox/Week_01/Pairwise.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
  */
 public class Pairwise {
     public static void main(String[] args) {
+        Pairwise.pairwiseBySequence();
+    }
+
+    private static void pairwiseWithSort(){
         Scanner sc = new Scanner(System.in);
         final Integer SIZE = sc.nextInt();
         Long[] a = new Long[SIZE];
@@ -16,5 +20,26 @@ public class Pairwise {
         }
         Arrays.sort(a);
         System.out.println(a[SIZE-1]*a[SIZE-2]);
+    }
+
+    private static void pairwiseBySequence(){
+        Scanner sc = new Scanner(System.in);
+        final Integer SIZE = sc.nextInt();
+
+        long maxValue = -1;
+        long preMaxValue = -1;
+
+        for (int i = 0; i < SIZE; i++) {
+            long readableValue = sc.nextLong();
+            if(readableValue >= preMaxValue){
+                if(readableValue >= maxValue){
+                    preMaxValue = maxValue;
+                    maxValue = readableValue;
+                }else {
+                    preMaxValue = readableValue;
+                }
+            }
+        }
+        System.out.println(maxValue*preMaxValue);
     }
 }
